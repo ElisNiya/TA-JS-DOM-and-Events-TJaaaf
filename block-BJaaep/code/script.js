4abe08cd8388bf7e8136677e3d85311666c97b75
ElisNiya <50265309+ElisNiya@users.noreply.github.com>
2021-11-11 08:36:07 +0100

    Update script.js

---
+++ block-BJaaep/code/script.js
@@ -0,0 +1,16 @@
+ <h2>Without Event Deligation</h2>
+        <ul class="boxes">
+          <li class="box">1</li>
+          <li class="box"></li>
+          <li class="box"></li>
+          <li class="box"></li>
+          <li class="box"></li>
+          <li class="box"></li>
+          <li class="box"></li>
+          <li class="box"></li>
+          <li class="box"></li>
+          <li class="box"></li>
+          <li class="box"></li>
+          <li class="box"></li>
+        </ul>
+      
